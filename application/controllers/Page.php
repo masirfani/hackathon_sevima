24e956db7fc3f212c8cec668b9bada2f3496b806
ma'sirfani <muhammadwalle123@gmail.com>
2020-09-12 08:25:28 +0700

    seting url dan tambah bootstrap 4

---
+++ application/controllers/Page.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Page extends CI_Controller {
+
+	public function index(){
+		$this->load->view('home');
+	}
+
+}
+
+/* End of file Page.php */
+/* Location: ./application/controllers/Page.php */
